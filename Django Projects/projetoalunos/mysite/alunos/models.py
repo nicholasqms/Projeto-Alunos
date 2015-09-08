3ff99e8b35cfb91fcda735fd91e2d97b77c687e7
nicholasqms <nquagliani@poli.ufrj.br>
2015-09-08 12:29:46 -0300

    implementation of the grades system

--- Django Projects/projetoalunos/mysite/alunos/models.py
+++ Django Projects/projetoalunos/mysite/alunos/models.py
@@ -16,6 +16,15 @@ class Student(models.Model):
 	def get_absolute_url(self):
             return reverse('student-detail', kwargs={'pk': self.pk})
 
+class Grades(models.Model)
+	grade_subject= models.Charfield(max_length=100)
+	student = models.ForeignKey(Student)
+	student_grade = models.DecimalField(max_digits=4, decimal_places=2)
+    	def __str__(self):              # __unicode__ on Python 2
+        	return self.name
+	def get_absolute_url(self):
+            return reverse('grade-detail', kwargs={'pk': self.pk})
+
 class Teacher(models.Model):
 	name = models.CharField(max_length=100)
 	birth_date = models.DateField(blank=True, null=True)
