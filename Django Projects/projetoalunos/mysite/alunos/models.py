2988ed03d65a84a55a3e747e102de753fb16ac1c
nicholasqms <nquagliani@poli.ufrj.br>
2015-09-09 06:16:47 -0300

    implementacao da criacao de um usuario aluno e inicio da funcao login

--- Django Projects/projetoalunos/mysite/alunos/models.py
+++ Django Projects/projetoalunos/mysite/alunos/models.py
@@ -1,5 +1,5 @@
 import datetime
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, BaseUserManager, AbstractBaseUser
 from django.core.urlresolvers import reverse
 from django.db import models
 from django.forms import ModelForm
@@ -12,10 +12,12 @@ class Student(models.Model):
     	birth_date = models.DateField(blank=True, null=True)
 	dre = models.PositiveIntegerField(blank=True, null= True)
 	course = models.CharField(max_length=100)
+	
     	def __str__(self):              # __unicode__ on Python 2
         	return self.name
 	def get_absolute_url(self):
             return reverse('student-list')
+#	pk = self.pk
 #            return reverse('student-list', kwargs={'pk': self.pk})
 
 class Grade(models.Model):
@@ -46,3 +48,57 @@ class TeacherForm(ModelForm):
 	class Meta:
 	  model = Teacher
 	  fields = ['name','birth_date','department']
+
+
+class StudentUser(AbstractBaseUser):
+    name = models.CharField(
+        verbose_name='Student\'s Name',
+        max_length=255,
+        unique=True,
+    )
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    birth_date = models.DateField(blank=True, null=True)
+    dre = models.PositiveIntegerField(blank=True, null= True)
+    course = models.CharField(max_length=100)
+    def __str__(self):              # __unicode__ on Python 2
+                return self.name
+    def get_absolute_url(self):
+            return reverse('student-list')
+#            return reverse('student-list', kwargs={'pk': self.pk})
+
+
+#    objects = MyUserManager()
+
+    USERNAME_FIELD = 'name'
+    REQUIRED_FIELDS = ['birth_date','dre','course']
+
+    def get_full_name(self):
+        # The user is identified by their email address
+        return self.name
+
+#    def get_short_name(self):
+        # The user is identified by their email address
+#        return self.email
+
+ #   def __str__(self):              # __unicode__ on Python 2
+  #      return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        #return self.is_admin
+	return False
+
+
--- Django Projects/projetoalunos/mysite/alunos/views.py
+++ Django Projects/projetoalunos/mysite/alunos/views.py
@@ -5,10 +5,11 @@ from .models import Student, Teacher, Grade
 from .models import StudentForm, TeacherForm
 from django.contrib.auth.models import User
 from django import forms
-from forms import StudentAddForm
-from django.contrib.auth import login
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from forms import StudentAddForm, StudentUserCreationForm
+from django.contrib.auth import login,authenticate
+from django.http import HttpResponseRedirect,HttpResponse
+from django.shortcuts import render,render_to_response
+from django.template import RequestContext
 # Create your views here.
 
 class StudentList(ListView):
@@ -58,7 +59,63 @@ def StudentAddUser(request):
 
     return render(request, 'adduser.html', {'form': form}) 
 
+def StudentCreateUser(request):
+    if request.method == "POST":
+        form = StudentUserCreationForm(request.POST)
+        if form.is_valid():
+#            new_user = User.objects.create_user(StudentUser.self,StudentUser.name,StudentUser.birth_date,StudentUser.dre,StudentUser.course)
+ 	   user = form.save()
+	   user.set_password(user.password)
+	   user.save()
+           login(request,user)
+            # redirect, or however you want to get to the main view
+           return reverse_lazy('student-list')
+    else:
+        form = StudentUserCreationForm()
 
+    return render(request, 'createuser.html', {'form': form})
+
+def StudentUserLogin(request):
+    # Like before, obtain the context for the user's request.
+    context = RequestContext(request)
+
+    # If the request is a HTTP POST, try to pull out the relevant information.
+
+
+    if request.method == 'POST':
+        # Gather the username and password provided by the user.
+        # This information is obtained from the login form.
+        username = request.POST['username']
+        password = request.POST['password']
+
+        # Use Django's machinery to attempt to see if the username/password
+        # combination is valid - a User object is returned if it is.
+        user = authenticate(username=username, password=password)
+
+        # If we have a User object, the details are correct.
+        # If None (Python's way of representing the absence of a value), no user
+        # with matching credentials was found.
+        if user:
+            # Is the account active? It could have been disabled.
+            if user.is_active:
+                # If the account is valid and active, we can log the user in.
+                # We'll send the user back to the homepage.
+                login(request, user)
+                return reverse_lazy('main')
+            else:
+                # An inactive account was used - no logging in!
+                return HttpResponse("Your account is disabled.")
+        else:
+            # Bad login details were provided. So we can't log the user in.
+            print "Invalid login details: {0}, {1}".format(username, password)
+            return HttpResponse("Invalid login details supplied.")
+
+    # The request is not a HTTP POST, so display the login form.
+    # This scenario would most likely be a HTTP GET.
+    else:
+        # No context variables to pass to the template system, hence the
+        # blank dictionary object...
+        return render_to_response('login.html', {}, context)
 #class UserCreationForm(forms.ModelForm)
 #"""
 #    A form that creates a user, with no privileges, from the given username and
